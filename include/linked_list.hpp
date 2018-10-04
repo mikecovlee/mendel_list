746fc22c2511327ed9c2966667d0a94c80bfc171
Michael Lee <mikecovlee@163.com>
2018-10-04 21:18:15 +0800

    修复Bug, 格式化文件

--- include/linked_list.hpp
+++ include/linked_list.hpp
@@ -1,36 +1,38 @@
 #pragma once
-#include <iostream>
 #include <stdexcept>
 #include <ostream>
 #include <utility>
 #include <array>
 namespace mendel {
-	using size_t=unsigned long;
-	template <typename T> class node final {
+	using size_t = unsigned long;
+	template <typename T>
+	class node final {
 	public:
 		T data;
-		node *next=nullptr;
-		node()=default;
-		node(node* next_node):next(next_node) {}
-		template<typename element_t>node(element_t&& e, node *n = nullptr):data(std::forward<element_t>(e)), next(n) {}
+		node *next = nullptr;
+		node() = default;
+		node(node *next_node) : next(next_node) {}
+		template <typename element_t>
+		node(element_t &&e, node *n = nullptr) : data(std::forward<element_t>(e)), next(n) {}
 	};
-	template <typename T> class linked_list final {
+	template <typename T>
+	class linked_list final {
 	public:
-		using node_t=node<T>;
-		node_t *data=nullptr;
-		linked_list()=default;
-		linked_list(const linked_list& list):data(list.copy()) {}
-		linked_list(linked_list&& list) noexcept
+		using node_t = node<T>;
+		node_t *data = nullptr;
+		linked_list() = default;
+		linked_list(const linked_list &list) : data(list.copy()) {}
+		linked_list(linked_list &&list) noexcept
 		{
-			std::swap(data,list.data);
+			std::swap(data, list.data);
 		}
-		template<std::size_t length>
-		linked_list(const std::array<T,length>& arr)
+		template <std::size_t length>
+		linked_list(const std::array<T, length> &arr)
 		{
-			node_t* new_data=new node_t(arr[0]);
-			data=new_data;
-			for(size_t i=1; i<length; new_data=new_data->next, ++i)
-				new_data->next=new node_t(arr[i]);
+			node_t *new_data = new node_t(arr[0]);
+			data = new_data;
+			for (size_t i = 1; i < length; new_data = new_data->next, ++i)
+				new_data->next = new node_t(arr[i]);
 		}
 		~linked_list()
 		{
@@ -38,102 +40,102 @@ namespace mendel {
 		}
 		bool empty() const
 		{
-			return data==nullptr;
+			return data == nullptr;
 		}
 		void clear()
 		{
 			while (data != nullptr) {
-				node_t* curr = data;
+				node_t *curr = data;
 				data = data->next;
 				delete curr;
 			}
-			data=nullptr;
+			data = nullptr;
 		}
-		node_t* copy() const
+		node_t *copy() const
 		{
-			if(data==nullptr)
+			if (data == nullptr)
 				return nullptr;
-			node_t* new_data=new node_t(data->data);
-			for(node_t* e=data->next,*it=new_data; e!=nullptr; e=e->next, it=it->next)
-				it->next=new node_t(e->data);
+			node_t *new_data = new node_t(data->data);
+			for (node_t *e = data->next, *it = new_data; e != nullptr; e = e->next, it = it->next)
+				it->next = new node_t(e->data);
 			return new_data;
 		}
-		T& max()
+		T &max()
 		{
-			if(data==nullptr)
+			if (data == nullptr)
 				throw std::logic_error("Container is empty.");
-			node_t* dat=data;
-			for(node_t* it=data->next; it!=nullptr; it=it->next)
-				if(it->data>dat->data)
-					dat=it;
+			node_t *dat = data;
+			for (node_t *it = data->next; it != nullptr; it = it->next)
+				if (it->data > dat->data)
+					dat = it;
 			return dat->data;
 		}
-		const T& max() const
+		const T &max() const
 		{
-			return const_cast<linked_list*>(this)->max();
+			return const_cast<linked_list *>(this)->max();
 		}
 		void reserve()
 		{
-			if(data!=nullptr) {
-				node_t* new_data=new node_t(data->data);
-				for(node_t* it=data->next; it!=nullptr; it=it->next)
-					new_data=new node_t(it->data, new_data);
+			if (data != nullptr) {
+				node_t *new_data = new node_t(data->data);
+				for (node_t *it = data->next; it != nullptr; it = it->next)
+					new_data = new node_t(it->data, new_data);
 				clear();
-				data=new_data;
+				data = new_data;
 			}
 		}
-		T& locate(size_t index)
+		T &locate(size_t index)
 		{
-			node_t* dat=data;
-			for(size_t i=0; dat!=nullptr; dat=dat->next,++i)
-				if(i==index)
+			node_t *dat = data;
+			for (size_t i = 0; dat != nullptr; dat = dat->next, ++i)
+				if (i == index)
 					return dat->data;
 			throw std::out_of_range("Index out of range.");
 		}
-		const T& locate(size_t index) const
+		const T &locate(size_t index) const
 		{
-			const_cast<linked_list*>(this)->locate(index);
+			const_cast<linked_list *>(this)->locate(index);
 		}
 		void tidy_up()
 		{
-			std::cout<<__func__<<":"<<*this<<std::endl;
-			node_t* target=nullptr;
-			for(node_t* e=data; e!=nullptr; e=e->next) {
-				for(node_t* it=e; it!=nullptr; it=it->next) {
-					if(e->data==it->data) {
-						target=e;
+			node_t *target = nullptr;
+			for (node_t *it = data; it != nullptr; it = it->next) {
+				for (node_t *matched = it->next; matched != nullptr; matched = matched->next) {
+					if (matched->data == it->data) {
+						target = it;
 						break;
 					}
 				}
-				if(target!=nullptr)
+				if (target != nullptr)
 					break;
 			}
-			if(target!=nullptr) {
-				for(node_t *previous=target, *it=target->next; it!=nullptr;) {
-					if(target->data==it->data) {
-						previous->next=it->next;
+			if (target != nullptr) {
+				for (node_t *previous = target, *it = target->next; it != nullptr;) {
+					if (target->data == it->data) {
+						previous->next = it->next;
 						delete it;
-						it=previous->next;
+						it = previous->next;
 					}
 					else {
-						previous=previous->next;
-						it=it->next;
+						previous = previous->next;
+						it = it->next;
 					}
 				}
 				tidy_up();
 			}
 		}
-		void merge(const linked_list& list)
+		void merge(const linked_list &list)
 		{
-			if(&list==this)
+			if (&list == this)
 				throw std::logic_error("Self merge.");
-			if(data!=nullptr) {
-				node_t* tail=data;
-				for(; tail->next!=nullptr; tail=tail->next);
-				tail->next=list.copy();
+			if (data != nullptr) {
+				node_t *tail = data;
+				for (; tail->next != nullptr; tail = tail->next)
+					;
+				tail->next = list.copy();
 			}
 			else
-				data=list.copy();
+				data = list.copy();
 			sort();
 		}
 		void sort()
@@ -141,13 +143,13 @@ namespace mendel {
 			// TODO
 		}
 	};
-}
-template<typename T>
-std::ostream& operator<<(std::ostream& o,const mendel::linked_list<T>& list)
+} // namespace mendel
+template <typename T>
+std::ostream &operator<<(std::ostream &o, const mendel::linked_list<T> &list)
 {
-	using node_t=typename mendel::linked_list<T>::node_t;
-	for(node_t* it=list.data; it!=nullptr; it=it->next)
-		o<<it->data<<"\t";
-	o<<std::flush;
+	using node_t = typename mendel::linked_list<T>::node_t;
+	for (node_t *it = list.data; it != nullptr; it = it->next)
+		o << it->data << "\t";
+	o << std::flush;
 	return o;
 }
--- test_linked_list.cpp
+++ test_linked_list.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <array>
-#include"linked_list.hpp"
+#include "linked_list.hpp"
 int main()
 {
-	std::array<int,6> a = { 1,2,3,4,5,6 };
-	std::array<int,8> b = { 8,7,6,5,4,3,2,1 };
+	std::array<int, 6> a{{1, 2, 3, 4, 5, 6}};
+	std::array<int, 8> b{{8, 7, 6, 5, 4, 3, 2, 1}};
 	mendel::linked_list<int> p(a);
 	mendel::linked_list<int> q(b);
-	std::cout<<p<<std::endl;
-	std::cout<<q<<std::endl;
-	std::cout<<p.max()<<std::endl;
-	std::cout<<p.locate(3)<<std::endl;
+	std::cout << p << std::endl;
+	std::cout << q << std::endl;
+	std::cout << p.max() << std::endl;
+	std::cout << p.locate(3) << std::endl;
 	p.merge(q);
-	std::cout<<p<<std::endl;
+	std::cout << p << std::endl;
 	p.tidy_up();
-	std::cout<<p<<std::endl;
+	std::cout << p << std::endl;
 	return 0;
 }
